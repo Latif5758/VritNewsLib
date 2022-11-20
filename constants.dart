121901f78cb4be8d6a123ef0b32b91769a0823b3
Latif5758 <latifikhwanulmuttaqin@gmail.com>
2022-11-20 22:40:16 +0700

    1.2

---
+++ constants.dart
@@ -0,0 +1,1 @@
+const String apiKey = '488eefa610d14d818b2597b33e2ef2c7';
---
+++ list_kategori.dart
@@ -0,0 +1,23 @@
+const List<Map<String, String>> listOfCountry = [
+  {'name': 'USA', 'code': 'us'},
+  {'name': 'UNITED KINGDOM', 'code': 'gb'},
+  {'name': 'Indonesia', 'code': 'id'},
+];
+
+const List<Map<String, String?>> listOfCategory = [
+  {'name': 'science', 'code': 'science'},
+  {'name': 'business', 'code': 'business'},
+  {'name': 'technology', 'code': 'technology'},
+  {'name': 'sports', 'code': 'sports'},
+  {'name': 'health', 'code': 'health'},
+  {'name': 'general', 'code': 'general'},
+  {'name': 'entertainment', 'code': 'entertainment'},
+  {'name': 'ALL', 'code': null},
+];
+const List<Map<String, String>> listOfNewsChannel = [
+  {'name': 'BBC News', 'code': 'bbc-news'},
+  {'code': 'the-washington-post', 'name': 'The Washington Post'},
+  {'code': 'cnn', 'name': 'cnn'},
+  {'code': 'nbc-news', 'name': 'nbc news'},
+  {'code': 'fox-news', 'name': 'Fox News'},
+];
